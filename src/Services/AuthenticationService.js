04d0cef2e68c7991d4c0b42c03f09b2be1118bac
akashudl <78961715+akashudl@users.noreply.github.com>
2021-03-05 14:14:46 +0530

    Update AuthenticationService.js

--- src/Services/AuthenticationService.js
+++ src/Services/AuthenticationService.js
@@ -21,8 +21,8 @@ class AuthenticationService
    isUserAdmin()
    {
     let user=sessionStorage.getItem('autheticatedUser');
-    console.log(user);
-    if((user==="jayasurya.j@qwikcilver.com"||user==="pradeep.kumar@qwikcilver.com")&&user!=null)
+    console.log(user);'
+    if((user==="jayasurya.j@qwikcilver.com"||user==="pradeep.kumar@qwikcilver.com"||user==="santosh.mv@qwikcilver.com"||user==="aravind.rajasekhar@qwikcilver.com")&&user!=null)
     { 
         return  true;
 
