289e99a9680d966f35d006ffdb934d63a4950f49
akashudl <78961715+akashudl@users.noreply.github.com>
2021-03-05 16:03:51 +0530

    Add files via upload

---
+++ src/Services/AuthenticationService.js
@@ -0,0 +1,35 @@
+class AuthenticationService
+{
+   registerSucessfullLogin(username)
+   {
+       console.log("RegisterSuceffsul Login")
+       sessionStorage.setItem('autheticatedUser',username);
+   }
+   logout()
+   {
+    sessionStorage.removeItem('autheticatedUser');
+   }
+  
+   isUserloggedin()
+   {
+       let user=sessionStorage.getItem('autheticatedUser');
+       if(user===null)
+       return false;
+       else
+       return true;
+   }
+   isUserAdmin()
+   {
+    let user=sessionStorage.getItem('autheticatedUser');
+    console.log(user);
+    if((user==="jayasurya.j@qwikcilver.com"||user==="pradeep.kumar@qwikcilver.com"||user==="akash.pandit_intern@qwikcilver.com")&&user!=null)
+    { 
+        return  true;
+
+    }
+    else 
+    return false ;
+   }
+
+}
+export default new  AuthenticationService();
