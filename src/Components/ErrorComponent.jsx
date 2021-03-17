916d82ef7534391fd155049c4c9043974d1c3456
akashudl <78961715+akashudl@users.noreply.github.com>
2021-03-17 19:18:28 +0530

    Update ErrorComponent.jsx

--- src/Components/ErrorComponent.jsx
+++ src/Components/ErrorComponent.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 function ErrorComponent()
 {
-  return <div style={{textAlign:"center"},{color:"white"}}><b>An Error occured.Contact support Team of Qwikcilver</b></div>;
+  return <div style={{color:"white"}}><b>An Error occured.Contact support Team of Qwikcilver</b></div>;
 }
 export default ErrorComponent;
