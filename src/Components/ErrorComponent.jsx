3df87e526eee4ff1ba8b7f9598de4f4d16121c43
akashudl <78961715+akashudl@users.noreply.github.com>
2021-03-18 13:36:08 +0530

    Delete src directory

--- src/Components/ErrorComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ErrorComponent()
-{
-  return <div style={{color:"white"}}><b>An Error occured.Contact support Team of Qwikcilver</b></div>;
-}
-export default ErrorComponent;
